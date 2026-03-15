9b2a4d02f44658bba6db0ec27ea23456998b1086
Hissachi <lucashissachi@gmail.com>
2026-03-14 22:28:27 -0300

    feat(put-patch): http update function working on swagger

--- app/Http/Controllers/Api/ProdutoController.php
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Produto;
 use App\Http\Requests\StoreProdutoRequest;
-use Illuminate\Http\Request;
+use App\Http\Requests\UpdateProdutoRequest;
+use Illuminate\Http\Requests;
 
 class ProdutoController extends Controller
 {
@@ -36,14 +37,15 @@ class ProdutoController extends Controller
         return Produto::findOrFail($id);
     }
 
-    public function update(Request $request, $id)
+    public function update(UpdateProdutoRequest $request, $id)
     {
         $produto = Produto::findOrFail($id);
 
-        $produto->update($request->all());
+        $produto->update($request->validated());
 
-        return response()->json($produto);
+        return response()->json($produto, 201);
     }
+
 
     public function destroy($id)
     {
